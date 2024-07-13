4131c1ede05af5450a33c17f036329f740a79436
mirza <sh366200@dal.ca>
2024-07-12 23:45:06 -0300

    app1

--- app1/index1.js
+++ app1/index1.js
@@ -1,53 +1,54 @@
 const express = require('express');
 const axios = require('axios');
 const fs = require('fs');
-const path = require('path');
 
 const app = express();
 const port = 6000;
 
 app.use(express.json());
-const persistant_storage_path = '/app/shifa_PV_dir'
 
 app.post('/store-file', (req, res) => {
-    const { file, data } = req.body;
+  const { file, data } = req.body;
 
-    if (!file || !data) {
-        return res.status(400).json({ file: file, error: "Invalid JSON input." });
+  if (!file || !data || data==undefined) {
+    return res.status(400).json({ file: null, error: 'Invalid JSON input.' });
+  }
+
+  const filePath = `/app/shifa_PV_dir/${file}`; 
+  fs.writeFile(filePath, data, (err) => {
+    if (err) {
+      return res.status(500).json({ file, error: 'Error while storing the file to the storage.' });
     }
-
-    const filePath = path.join(persistant_storage_path, file);
-
-    fs.writeFile(filePath, data, (err) => {
-        if (err) {
-            return res.status(500).json({ file, error: "Error while storing the file to the storage." });
-        }
-        return res.json({ file, message: "Success." });
-    });
+    return res.json({ file, message: 'Success.' });
+  });
 });
 
+
 app.post('/calculate', async (req, res) => {
+  try {
     const { file, product } = req.body;
 
-    if (!file) {
-        return res.status(400).json({ file: file, error: "Invalid JSON input." });
+    if (!file || !product) {
+      return res.status(400).json({ file: null, error: 'Invalid JSON input.' });
     }
-
-    const filePath = path.join(persistant_storage_path, file);
+  
+    const filePath = `/app/shifa_PV_dir/${file}`;
 
     if (!fs.existsSync(filePath)) {
-        console.log("Containe 1:1");
-        return res.status(404).json({  file: file ,error: "File not found.",});
+      return res.status(400).json({ file, error: 'File not found.' });
     }
 
-    try {
-        const response = await axios.post('http://container2-service:6001/calculate', { file, product });
-        return res.json(response.data);
-    } catch (error) {
-        return res.status(500).json({ error: "Input file not in CSV format.", file });
-    }
+    const response = await axios.post('http://container2-service:6001/calculate', { file, product });
+    return res.json(response.data);
+  } catch (error) {
+    console.log('Error:', error);
+    return res.status(500).json({ file: error.response.data.file, error: error.response.data.error});
+  }
 });
 
 app.listen(port, () => {
-    console.log(`App1 listening on port ${port}`);
+  if (!fs.existsSync('/app/shifa_PV_dir')){
+    fs.mkdirSync('/app/shifa_PV_dir');
+  }
+  console.log(`Container 1 listening at http://localhost:${port}`);
 });
